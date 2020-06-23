8c9f36044185482e4d8768e61bfe7b6f9f2d62ff
Kaz Yoshikawa <kaz.yoshikawa@gmail.com>
2020-06-23 09:59:03 -0400

    updated to Swift5, Xcode 11

--- ZBitField/ZBitfield.swift
+++ ZBitField/ZBitfield.swift
@@ -4,7 +4,7 @@
 //
 //	The MIT License (MIT)
 //
-//	Copyright (c) 2016 Electricwoods LLC, Kaz Yoshikawa.
+//	Copyright (c) 2020 Electricwoods LLC, Kaz Yoshikawa.
 //
 //	Permission is hereby granted, free of charge, to any person obtaining a copy 
 //	of this software and associated documentation files (the "Software"), to deal 
